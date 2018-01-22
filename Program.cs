45d042b65ec61c4305f7ab91691989f05de5340e
Clément Rocher <clementrocher71@gmail.com>
2018-01-22 16:18:32 +0100

    main

--- Program.cs
+++ Program.cs
@@ -6,7 +6,13 @@ namespace ArtGallery
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Start!");
+            ArtGalery galery = new ArtGalery();
+            galery.Initialize();
+            galery.Sort();
+            foreach (ArtPiece piece in galery.collection)  
+            {  
+                Console.Write(piece.ToString()); 
+            }
         }
     }
 }
