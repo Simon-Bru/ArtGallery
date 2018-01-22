30988f85b46a395543763dcb4bb3a02f3c329fc8
Clément Rocher <clementrocher71@gmail.com>
2018-01-22 15:13:40 +0100

    artgalery begin

--- ArtGalery.cs
+++ ArtGalery.cs
@@ -2,4 +2,21 @@ using System;
 
 class ArtGalery
 {
+    private List<ArtPiece> m_Collection; 
+
+    public int data // public property
+    {
+        get { return m_data; } // access control
+        set { m_data = value; }
+    }
+
+    public ArtGalery Initialize ()
+    {
+        List<ArtPiece> m_Collection = new List<ArtPiece>();
+    }
+
+    public Art ProcessArtPiece()
+    {
+        
+    }
 }
